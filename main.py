eec01cef27ffea48f55acef7031ffbf754ae19f1
Dylan Castro Zorrilla <37878046+Dylan-Castro@users.noreply.github.com>
2021-08-26 05:25:23 +0000

    El reproductor funciona con link y textos

--- main.py
+++ main.py
@@ -1,14 +1,71 @@
-import discord
+import youtubeUtil, discord
 from discord.ext import commands,tasks
 import os
 from dotenv import load_dotenv
-import youtube_dl
+
 
 load_dotenv()
 
 # Get the API token from the .env file.
 DISCORD_TOKEN = os.environ['discord_token']
+FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
 
 intents = discord.Intents().all()
 client = discord.Client(intents=intents)
-bot = commands.Bot(command_prefix='!',intents=intents)
+bot = commands.Bot(command_prefix='-/',intents=intents)
+
+#Lista de Eventos
+@bot.event
+async def on_ready():
+    print("GroovyTEC está ready!")
+
+# Lista de Comandos
+@bot.command(name='test', help='Pa testear mi pana')
+async def test(ctx):
+    await ctx.send("Ta fino mi rey.")
+
+@bot.command(name='join', help='Tells the bot to join the voice channel')
+async def join(ctx):
+    if not ctx.message.author.voice:
+        await ctx.send("{} is not connected to a voice channel".format(ctx.message.author.name))
+        return
+    else:
+        channel = ctx.message.author.voice.channel
+    await channel.connect()
+
+@bot.command(name='l', help='Para hacer que se quite del canal')
+async def leave(ctx):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_connected():
+        await voice_client.disconnect()
+    else:
+        await ctx.send("No estoy conectado al canal de voz mi chamo.")
+
+@bot.command(name='p', help='Para reproducir una cancion')
+async def play(ctx,*url):
+    try :
+      cancion = ""
+      if type(url) == tuple:
+        for palabra in url:
+          cancion += palabra
+          cancion += " "
+      else:
+        cancion = url
+
+      voice_channel = ctx.author.voice.channel
+      voice = ctx.channel.guild.voice_client
+      if voice is None:
+          voice = await voice_channel.connect()
+      elif voice.channel != voice_channel:
+          voice.move_to(voice_channel)
+
+      async with ctx.typing():
+          filename = await youtubeUtil.YTDLSource.from_url(cancion, loop=bot.loop, stream=True)
+          voice.play(filename)
+      await ctx.send('**Sonando para tí mi king:**\n {}'.format(filename.title))
+    except:
+        await ctx.send("No se pudo reproducir la cancion mi king.")
+
+
+if __name__ == "__main__" :
+    bot.run(DISCORD_TOKEN)
---
+++ youtubeUtil.py
@@ -0,0 +1,47 @@
+import youtube_dl,discord,asyncio,requests
+youtube_dl.utils.bug_reports_message = lambda: ''
+
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
+}
+
+#FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+
+ffmpeg_options = {
+    'options': '-vn'
+}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+        self.data = data
+        self.title = data.get('title')
+        self.url = ""
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+
+        if 'entries' in data:
+            # take first item from a playlist
+            data = data['entries'][0]
+
+        filename = data['url'] if stream else ytdl.prepare_filename(data)
+        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
+    
+    #Get videos from links or from youtube search
+    def search(query):
+      info = ytdl.extract_info(query, download=False)
+      return info['formats'][0]['url']
